d2531301f2f459cd5e24f1877c0cf7672e13a05d
heldergoncalves92 <heldergoncalves92@gmail.com>
2015-06-07 20:17:46 +0100

    With Siblings - Manual

--- PI_Start/PI_Start/ENUM.cpp
+++ PI_Start/PI_Start/ENUM.cpp
@@ -75,33 +75,32 @@ void startSet(int id, Enum set){
     
     
     //Prepare to start by type
-    if(set->type == 1){
+    if(set->type == 0){
+        uT[id][0] = 1;
+        for(i=1; i<=bound; i++)
+            cT[id][i] = 0.0;
+        
+    } else if(set->type == 1){
+        //Base Start
         cT[id][bound] = 0.0;
         cT[id][bound-1] = B[bound-1];
         uT[id][bound-1] = 1;
-        uT[id][0] = 1;
-        
-        for(i=bound-1; i>=0; i--)
-            cT[id][i] = cT[id][i + 1] + (y[id][i]*y[id][i] + 2*uT[id][i]*y[id][i] + uT[id][i]*uT[id][i]) * B[i];
         
     } else
-        if(set->type == 2){
+        if(set->type == 2 || set->type == 3){
             cT[id][bound] = 0.0;
             cT[id][bound-1] = B[bound-1];
             uT[id][bound-1] = 1;
-            uT[id][0] = 1;
+            
             uT[id][bound-2] = set->sibling;
+            delta[id][bound-2] = set->sibling;
             
             
-            for(i=bound-1; i>=0; i--)
+            for(i=bound-2; i>= bound - set->level-2; i--)
                 cT[id][i] = cT[id][i + 1] + (y[id][i]*y[id][i] + 2*uT[id][i]*y[id][i] + uT[id][i]*uT[id][i]) * B[i];
            // set->bound--;
             
-        } else{
-            uT[id][0] = 1;
-            for(i=0; i<=bound; i++)
-                cT[id][i] = 0.0;
-    }
+        }
 }
 
 
@@ -133,6 +132,11 @@ Enum pop(){
     //Lock critical zone
     pthread_mutex_lock(&toPop_Mutex);
     
+    if(!list->head){
+        pthread_mutex_unlock(&toPop_Mutex);
+        return NULL;
+    }
+        
     aux = list->head;
     list->head = list->head->next;
     list->count--;
@@ -143,23 +147,148 @@ Enum pop(){
     return aux;
 }
 
+void moveDown(int id, int bound, int s, int times){
+    
+    int i, t = bound;
+    
+    double aux;
+  //  printVec(dim, id);
+    
+    while(times>0){
+        times--;
+        //moveDown
+        t--;
+        aux = 0;
+        
+        for (i = t + 1; i <= s; i++){
+            aux += uT[id][i] * mu[i][t];
+        }
+        
+        y[id][t] = aux;
+        uT[id][t] = v[id][t] = int(round(-aux));
+        delta[id][t] = 0;
+        
+        if (uT[id][t] > -aux){
+            d[id][t] = -1;
+        }
+        else{
+            d[id][t] = 1;
+        }
+        
+        //Prepare cT[t] to next iteration
+        cT[id][t] = cT[id][t + 1] + (y[id][t]*y[id][t] + 2*uT[id][t]*y[id][t] + uT[id][t]*uT[id][t]) * B[t];
+    //    printVec(dim, id);
+    }
+
+}
+
+
+void moveDownUP(int id, int bound, int s, int times){
+    
+    int i, t = bound;
+    
+    double aux;
+    printVec(dim, id);
+    
+    while(times>0){
+        times--;
+        //moveDown
+        t--;
+        aux = 0;
+        
+        for (i = t + 1; i <= s; i++){
+            aux += uT[id][i] * mu[i][t];
+        }
+        
+        y[id][t] = aux;
+        uT[id][t] = v[id][t] = int(round(-aux));
+        delta[id][t] = 0;
+        
+        if (uT[id][t] > -aux){
+            d[id][t] = -1;
+        }
+        else{
+            d[id][t] = 1;
+        }
+        
+        //Prepare cT[t] to next iteration
+        cT[id][t] = cT[id][t + 1] + (y[id][t]*y[id][t] + 2*uT[id][t]*y[id][t] + uT[id][t]*uT[id][t]) * B[t];
+        printVec(dim, id);
+        
+        //moveUp
+        t++;
+        s = (s<t)?t:s; //Get max value
+        
+        if(t < s){
+            delta[id][t] = -delta[id][t];
+        }
+        if(delta[id][t]*d[id][t] >= 0){
+            delta[id][t] += d[id][t];
+        }
+        uT[id][t] = v[id][t] + delta[id][t];
+        
+        //Prepare cT[t] to next iteration
+        cT[id][t] = cT[id][t + 1] + (y[id][t]*y[id][t] + 2*uT[id][t]*y[id][t] + uT[id][t]*uT[id][t]) * B[t];
+        printVec(dim, id);
+    }
+    
+}
+
 
 
 //ENUM accordingly C. P. Schnorr && M. Euchner
 void EnumSET(Enum set, int id){
     
     double aux;
-    int s, t, i;
-    int toCopy = set->bound + 1;
+    int s, t, i, bound = set->bound;
+    int toCopy = bound + 1;
     
     startSet(id, set);
-    s = t = 0;
 
-    int bound = set->bound;
-    cT[id][t] = cT[id][t + 1] + (y[id][t]*y[id][t] + 2*uT[id][t]*y[id][t] + uT[id][t]*uT[id][t]) * B[t];
-    
-    while(t <= bound){
+    //Start on leaf (like Schnorr)
+    if(set->type==0){
+        s = t = 0;
+        cT[id][t] = cT[id][t + 1] + (y[id][t]*y[id][t] + 2*uT[id][t]*y[id][t] + uT[id][t]*uT[id][t]) * B[t];
+
+    //One Gamma
+    } else if(set->type == 1){
+        s = t = bound;
+
+    //Starts on given sibling and go on
+    } else if(set->type == 2){
+        s = bound;
+        bound = bound - set->level;
+        t=bound;
         
+    //Search just in one sibling
+    } else {
+        s = bound;
+        bound = bound - set->level;
+        moveDown(id, bound, s, 1);
+        bound--;
+        t=bound;
+
+    }
+
+    
+   /*
+    moveDown(id, bound, s, 1);
+    
+    bound--;
+    t = bound;
+
+    printVec(dim, id);
+    moveDownUP(id, bound, s, 1);
+    
+    printVec(dim, id);
+    */
+/*
+
+    cT[id][t] = cT[id][t + 1] + (y[id][t]*y[id][t] + 2*uT[id][t]*y[id][t] + uT[id][t]*uT[id][t]) * B[t];
+    printVec(dim, id);
+  */
+    while(t <= bound){
+       //printVec(dim, id);
         if (cT[id][t] < cL){
             if (t > 0){
                 //moveDown
@@ -187,8 +316,8 @@ void EnumSET(Enum set, int id){
             }
             else{
                 //UpdateVector
-                printf("%d:UPDATE\n",s);
-                //printVec(set);
+                printf("%d:UPDATE-%d\n",s, set->type);
+                //printVec(dim, id);
                 
                 //Lock critical zone
                 pthread_mutex_lock(&u_Mutex);
@@ -229,8 +358,10 @@ void* threadHander(void* vID){
     
     while (list->count>0) {
         set = pop();
-        printf("%d - %d\n",id, set->bound);
-        EnumSET(set, id);
+        if(set){
+            printf("%d - %d\n",id, set->bound);
+            EnumSET(set, id);
+        }
     }
 
     return NULL;
@@ -239,25 +370,45 @@ void* threadHander(void* vID){
 
 int* ENUM(){
     
-    int i, n=1, MAX_DEPTH = dim*0.7;
+    int i, n=1, MAX_DEPTH = dim*0.8;
     Enum set = NULL;
     
+    set = newEnumElem(55, 0, 3, 1);
+    addTail(set);
+    n++;
+    set = newEnumElem(55, 1, 3, 1);
+    addTail(set);
+    n++;
+    set = newEnumElem(55, -1, 3, 1);
+    addTail(set);
+    n++;
+    set = newEnumElem(55, 2, 2, 1);
+    addTail(set);
+    n++;
+    
+    set = newEnumElem(54, 0, 3, 1);
+    addTail(set);
+    n++;
+    set = newEnumElem(54, 1, 3, 1);
+    addTail(set);
+    n++;
+    set = newEnumElem(54, -1, 3, 1);
+    addTail(set);
+    n++;
+    set = newEnumElem(54, 2, 2, 1);
+    addTail(set);
+    n++;
+    
+    
+    
+    
     //Prepare list with tasks
-    for(i=dim-1; i>MAX_DEPTH; i--){
-        set = newEnumElem(i, 87, 2, 1);
+    for(i=dim-2; i>MAX_DEPTH; i--){
+        set = newEnumElem(i, 1, 1, 1);
         addTail(set);
         n++;
-        /*set = newEnumElem(i, 1, 2, 1);
-        addTail(set);
-        n++;
-        set = newEnumElem(i, -1, 2, 1);
-        addTail(set);
-        n++;
-        set = newEnumElem(i, -2, 1, 1);
-        addTail(set);
-        n++;*/
     }
-    set = newEnumElem(i, 1, 0,1);
+    set = newEnumElem(i, 1, 0, 1);
     addTail(set);
     
     
@@ -270,7 +421,6 @@ int* ENUM(){
         pthread_create(&tHandles[i], NULL, threadHander, (void *)threadNum);
 
     }
-    
     
     //Threads Join
     for (i = 0; i < nthreads; i++)
@@ -300,19 +450,19 @@ void printVec(int bound, int id){
         printf("%d ",delta[id][i]);
     printf("\n");
     
-    printf("v:");
+  /*  printf("v:");
     for(i=0; i<dimension; i++)
         printf("%d ",v[id][i]);
     printf("\n");
-    
+    */
     printf("cT:");
     for(i=0; i<dimension; i++)
         printf("%.2f ",cT[id][i]);
     printf("\n");
-    printf("Y:");
+  /*  printf("Y:");
     for(i=0; i<dimension; i++)
         printf("%.2f ",y[id][i]);
     printf("\n");
-    
+    */
     printf("\n");
 }
